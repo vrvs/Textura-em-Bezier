fdba6b07a9c039e3ba5c27831252683b1cb1baff
Lucas Alves Rufino <lar@cin.ufpe.br>
2017-07-17 18:14:15 +0000

    Bases do projeto - GUI, Grid e Entradas de arquivos

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+var width;
+var height;
+var canvas;
+var ctx;
+
+function initCanvas(){
+	width = document.getElementById('main').offsetWidth;
+	height = document.getElementById('main').offsetHeight;
+	canvas = document.getElementById('canvas');
+	ctx = canvas.getContext('2d');
+
+	canvas.width = width;
+	canvas.height = height;
+};
+
+initCanvas();
+initGrid();
